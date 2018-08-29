554321f849667f1cc8f1a29e9ed5b7d53fcc5588
master <elmurodmailbox@gmail.com>
2018-08-28 21:08:44 -0400

    How to handle frames with Selenium

---
+++ OnlineTraining/FrameHandling.java
@@ -0,0 +1,36 @@
+package OnlineTraining;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FrameHandling {
+
+	public static void main(String[] args) throws InterruptedException {
+	
+		System.setProperty("webdriver.chrome.driver", "/Users/elmurotyangiboev/Documents/Selenium dependencies/drivers/chromedriver");
+		WebDriver driver = new ChromeDriver(); // launch Chrome
+		
+		driver.manage().window().maximize(); // maximize window
+		driver.manage().deleteAllCookies(); // delete all the cookies
+		
+		//dynamic wait
+		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+		driver.get("https://www.freecrm.com/"); // enter url
+		
+		driver.findElement(By.name("username")).sendKeys("naveenk");
+		driver.findElement(By.name("password")).sendKeys("test@123");
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//input[@type='submit']")).click();
+		Thread.sleep(3000);
+		driver.switchTo().frame("mainpanel");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//a[contains(text(),'Contacts')]")).click();
+
+	}
+
+}
